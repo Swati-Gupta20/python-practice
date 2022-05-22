389d8c2514faf302d8476e7b65c571fba557603e
Swati-Gupta20 <106022280+Swati-Gupta20@users.noreply.github.com>
2022-05-22 19:47:47 +0530

    Add files via upload

---
+++ file-1_variables.py
@@ -0,0 +1,36 @@
+variable_nav=54.8
+gurukul ="546"
+print (variable_nav,gurukul)
+print(type(gurukul))
+print(type(variable_nav))
+a=str(56)
+b=int(9)
+c=float(9)
+print(a,b,c)
+a="34"
+print(type(a))
+x = str(7)
+print(x)
+fruits=['apple','mango','banana']
+a,b,c=fruits
+print(a)
+print(b)
+print(c)
+print(fruits)
+print("Nav\ngurukul")
+print("Nav\ngurukul")
+print("Nav\ngurukul")
+x=45
+y='mat'
+print(x,y)
+x = "awesome"
+def myfunc():
+    x="fantastic"  
+    myfunc()         
+print("python is "+ x)
+a=324
+b=546
+print(a+b)
+
+
+
